7217144079f5ac625e169406c27157e04aa3eae2
DeathBySnail <DeathBySnail@gmail.com>
2021-12-01 11:13:24 +0000

    day 1 impl

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,10 +1,21 @@
-pub struct Data {
-    pub value: i32,
-}
+pub fn sonar_sweep(input: &[i32], sweep_range: usize) -> i32 {
+    let mut depth_increases = 0;
 
-impl Data {
-    pub fn new() -> Data {
-        let d = Data { value: 4 };
-        d
+    let mut prev_depth = input[0];
+
+    for i in 1..input.len() {
+        let mut accum_depth = input[i];
+        for j in 1..sweep_range {
+            if i + j < input.len() {
+                accum_depth += input[i + j];
+            }
+        }
+
+        if accum_depth > prev_depth {
+            depth_increases += 1;
+        }
+        prev_depth = accum_depth;
     }
+
+    depth_increases
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
+mod common;
 mod day_1;
 
 #[cfg(test)]
 mod tests {
-    use crate::day_1;
+    use super::common;
+
     #[test]
-    fn it_works() {
-        let d = day_1::Data::new();
-        assert_eq!(2 + 2, d.value);
+    fn day_1() {
+        use super::day_1;
+        use std::io::BufRead;
+        let file = common::buffer_read("./input/day_1.txt");
+        match file {
+            Ok(input) => {
+                let int_vec: Vec<i32> = input
+                    .lines()
+                    .map(|x| x.unwrap().parse::<i32>().unwrap())
+                    .collect();
+                let result = day_1::sonar_sweep(&int_vec, 1);
+                assert_eq!(1527, result);
+
+                let result_extra = day_1::sonar_sweep(&int_vec, 3);
+                assert_eq!(1575, result_extra);
+            }
+            Err(e) => panic!("error: {}", e),
+        }
     }
 }
