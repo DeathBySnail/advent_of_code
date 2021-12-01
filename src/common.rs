6364875814d5be2e37b6d6c910b8eca18cd730fd
DeathBySnail <DeathBySnail@gmail.com>
2021-12-01 15:52:34 +0000

    better file reading

--- src/common.rs
+++ src/common.rs
@@ -1,15 +1,1 @@
-use std::fs::File;
-use std::io;
-use std::path::Path;
 
-pub fn buffer_read<P>(filename: P) -> io::Result<io::BufReader<File>>
-where
-    P: AsRef<Path>,
-{
-    let file_result = File::open(filename);
-
-    match file_result {
-        Ok(file) => Ok(io::BufReader::new(file)),
-        Err(e) => Err(e),
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,16 @@ mod day_1;
 
 #[cfg(test)]
 mod tests {
-    use super::common;
-
     #[test]
     fn day_1() {
         use super::day_1;
-        use std::io::BufRead;
-        let file = common::buffer_read("./input/day_1.txt");
-        match file {
-            Ok(input) => {
-                let int_vec: Vec<u32> = input
-                    .lines()
-                    .map(|x| x.unwrap().parse::<u32>().unwrap())
-                    .collect();
-                let result = day_1::sonar_sweep(&int_vec, 1);
-                assert_eq!(1527, result);
+        let file = include_str!("../input/day_1.txt");
 
-                let result_extra = day_1::sonar_sweep(&int_vec, 3);
-                assert_eq!(1575, result_extra);
-            }
-            Err(e) => panic!("error: {}", e),
-        }
+        let int_vec: Vec<u32> = file.lines().map(|x| x.parse::<u32>().unwrap()).collect();
+        let result = day_1::sonar_sweep(&int_vec, 1);
+        assert_eq!(1527, result);
+
+        let result_extra = day_1::sonar_sweep(&int_vec, 3);
+        assert_eq!(1575, result_extra);
     }
 }
